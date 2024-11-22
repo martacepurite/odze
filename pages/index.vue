2a4d6514e1a2a9e40e7eaa9857b3a9bed9f7a881
Marta <martacepurite@gmail.com>
2024-11-22 15:20:41 +0200

    added pages

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Index page</h1>
+    <ul>
+        <li><NuxtLink to="/login">Ielogoties</NuxtLink></li>
+        <li><NuxtLink to="/register">Reģistrēties</NuxtLink></li>
+    </ul>
+</template>
---
+++ pages/register.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {ref} from 'vue'
+
+const email = ref('')
+const password = ref('')
+const password_again = ref('')
+const type = ref('')
+const organization = ref('')
+
+function register(event){
+    event.preventDefault()
+    alert(email.value)
+}
+</script>
+
+
+<template>
+  <h1>Reģistrācija</h1>
+  <form name="login_form" @submit="register">
+
+        E-pasts: <input v-model="email" /><br>
+        Parole: <input v-model="password" /><br>
+        Parole atkārtoti: <input v-model="password_again" /><br>
+        Organizācijas nosaukums: <input v-model="organization" /><br>
+        
+            Konta tips:
+            <select v-model="type">
+                <option>Organizācijas administrators</option>
+                <option>Standarta lietotājs</option>
+            </select><br>
+        
+
+    <input type="submit" value="Reģistrēties">
+  </form>
+</template>
